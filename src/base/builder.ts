91e61f44726a6f73cb2f2dbaa348ebb434a6e755
goxdeveloper <mastergox.goxco@gmail.com>
2021-12-31 04:23:11 +0000

    remove sc type

--- src/base/builder.ts
+++ src/base/builder.ts
@@ -7,37 +7,26 @@ export const builder = (client) => {
     //SETTINGS
 
     let guildID = commandList.settings['guildID']
-    let type = commandList.settings['type']
     let commands = commandList.list.filter(x => x.name !== '')
-    if (type === 'global') commands = commands.filter(x => x.folder !== 'admin')
-
+    
     let position = 0
     do {
         let data = new SlashCommandBuilder()
         .setName(commands[position].name)
         .setDescription(commands[position].description.replaceAll('**',''))
-        eval(`${type}${commands[position].action.charAt(0).toUpperCase()+commands[position].action.slice(1)}(data)`)
+        eval(`${commands[position].action.charAt(0).toUpperCase()+commands[position].action.slice(1)}(data)`)
         position++
     }
     while (position < commands.length)
 
     //FUNCTIONS
 
-    async function localAdd(data) {
+    async function Add(data) {
         await client.guilds.cache.get(guildID)?.commands.create(data)
     }
     
-    async function localRemove(data) {
+    async function Remove(data) {
         const command = await client.guilds.cache.get(guildID)?.commands.create(data)
         await client.guilds.cache.get(guildID)?.commands.delete(command.id)
-    }
-
-    async function globalAdd(data) {
-        await client.application?.commands.create(data)
-    }
-
-    async function globalRemove(data) {
-        const command = await client.application?.commands.create(data)
-        await client.application?.commands.delete(command.id)
     }
 }
