78dbeb18dd71b64bd9086d61f5f693e89779d6bc
goxdeveloper <mastergox.goxco@gmail.com>
2022-01-08 13:48:33 +0000

    add logs (not included)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,40 @@
 const Discord = require('discord.js')
 const { Intents } = require('discord.js')
+const dotenv = require('dotenv')
+dotenv.config()
 
 //CLIENT SETTINGS
 
 const client = new Discord.Client({
-    intents: [Intents.FLAGS.GUILDS]
+    intents: [Intents.FLAGS.GUILDS,Intents.FLAGS.GUILD_MEMBERS,Intents.FLAGS.GUILD_PRESENCES]
 })
 
-//CONNECTION
+//BOT LOGIN
 
-const connect = require('./base/connect.js')
-connect(client)
+client.login(process.env.TOKEN)
+    
+//ON READY
+
+client.on('ready', () => {
+    console.log(`Connected to ${client.user.username}`)
+    start()
+})
+
+//START
+
+async function start() {
+    
+    const handler = require('./base/handler.js')
+    handler(client)
+
+    const builder = require('./base/builder.js')
+    builder(client)
+
+    const status = require('./base/status.js')
+    status(client)
+
+    //NOT INCLUDED
+
+    const logs = require('./base/logs.js')
+    logs(client)
+}
